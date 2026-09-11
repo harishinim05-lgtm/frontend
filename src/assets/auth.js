4afa2b9ea14ff5bd2dbbe4bccfe71143512e4d66
Harishini <harishinim05@gmail.com>
2026-09-11 16:05:58 +0530

    Update frontend

--- src/assets/auth.js
+++ src/assets/auth.js
@@ -7,7 +7,7 @@ const API_URL='https://backend-716y.onrender.com';
 export const register=async(username,password)=>{
      
    try{
-         await axios.post(`${API_URL}register`,{username,password}) //send                              data to backend
+         await axios.post(`${API_URL}/register`,{username,password}) //send                              data to backend
     }catch(err){
         console.error('Error during login:', err.response ? err.response.data : err.message);
         throw new Error('Registration Failed')
@@ -23,7 +23,7 @@ export const register=async(username,password)=>{
 
 export const login=async(username,password)=>{
     try{
-        const response= await axios.post(`${API_URL}login`,{username,password})
+        const response= await axios.post(`${API_URL}/login`,{username,password})
         const token = response.data.token;
         localStorage.setItem('token', token);
         return token;
